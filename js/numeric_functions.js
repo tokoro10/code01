12fa9178a1f0bf0a982e763b077daffef2c3fc86
N.Shimizu <chikoski@gmail.com>
2016-04-07 08:48:24 +0900

    initial commit

---
+++ js/numeric_functions.js
@@ -0,0 +1,19 @@
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return add(a, -1 * b);
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function mod(a, b){
+  return a % b;
+}
